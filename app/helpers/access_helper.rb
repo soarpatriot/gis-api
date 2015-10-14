4aa5c7fbbec8bee17eb287c4cb43041da16bbde1
soar <liuhaibao@xiaojiuwo.cn>
2015-10-14 19:37:42 +0800

    doing the server auth

--- app/helpers/access_helper.rb
+++ app/helpers/access_helper.rb
@@ -9,11 +9,26 @@ module AccessHelper
     origin = env["HTTP_ORIGIN"]
     error!("4011", 401) if params[:api_key].nil?
     #origin = env["HTTP_ORIGIN"]
-    error!("4012", 401) if Key.where(origin: origin,api_key: params[:api_key]).first.nil?
+    key = Key.where(api_key: params[:api_key]).first
+    error!("4012", 401) if key.js? && key.origin != origin  
+
+    signature = params[:signature]
+
+    if key.server?
+      error!("4013", 401) unless sign(params, key.api_secret) == signature
+    end
+   
   end
 
   def current_user
     @current_user ||= AuthToken.where(value: params[:auth_token]).first.try(:user)
   end
+
+  def sign_params params,api_secret
+    sign_str = params.keys.sort.map{ |k,v| "#{k}=#{v}" if k != "signature" }.join("&") + api_secret
+    binding.pry
+    Digest::SHA1.hexdigest(sign_str)
+  end
+
 
 end
