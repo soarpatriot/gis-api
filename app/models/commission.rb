1c20a4f1de997d858a32754617c2cea18624f4d3
soar <liuhaibao@xiaojiuwo.cn>
2015-10-26 11:25:30 +0800

    add order by price asc

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -1,4 +1,7 @@
 class Commission < ActiveRecord::Base
+  
+  default_scope -> {order(price: :asc)}
+
   validates :name,:price  ,presence: true
   has_many :areas
 end
--- spec/apis/v1/commissions_api_spec.rb
+++ spec/apis/v1/commissions_api_spec.rb
@@ -12,6 +12,15 @@ describe V1::CommissionsApi do
       res = auth_json_get commissions_path 
       expect(res.size).to eq(5)
     end
+    it "order by price asc" do
+
+      create :commission, price: 0.8
+      create :commission, price: 0.3
+      create :commission, price: 0.5
+      res = auth_json_get commissions_path 
+      expect(res[0][:price]).to eq(0.3)
+    end
+
   end
 
 end
