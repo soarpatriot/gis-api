4a7d7e28966f994eb440cd4f76cf9808ae2dc4cf
Soar <85624529@qq.com>
2015-12-11 16:05:52 +0800

    add a new api for no order log

--- spec/apis/v2/areas_api_spec.rb
+++ spec/apis/v2/areas_api_spec.rb
@@ -4,6 +4,7 @@ describe V2::AreasApi do
 
   let(:areas_path) { "/v2/areas" }
   let(:areas_commission_path) { "/v2/areas/commission" }
+  let(:areas_price_path) { "/v2/areas/price" }
  
   def update_area_path area 
     "v2/areas/#{area.id}"
@@ -317,5 +318,147 @@ describe V2::AreasApi do
     end
   end    
 
+  context "get price" do 
+    it "success" do
+      point1 = create :point, lantitude: 40.0536, longitude: 116.297
+      point2 = create :point, lantitude: 40.0497, longitude: 116.298 
+      point3 = create :point, lantitude: 40.0464, longitude: 116.303 
+      point4 = create :point, lantitude: 40.0475, longitude: 116.311 
+      point5 = create :point, lantitude: 40.0511, longitude: 116.311 
+      point6 = create :point, lantitude: 40.0547, longitude: 116.308 
+      point7 = create :point, lantitude: 40.055, longitude: 116.307 
 
+      points = [point1,point2,point3,point4,point5,point6,point7]
+
+      test_point = Hash.new 
+      test_point[:lantitude] = 40.0521
+      test_point[:longitude] = 116.305
+      test_point2 = Hash.new 
+      test_point2[:lantitude] = 40.0536
+      test_point2[:longitude] = 116.297
+      test_point3 = Hash.new 
+      test_point3[:lantitude] = 40.046897
+      test_point3[:longitude] = 116.299642
+
+
+      points2 = create_list :point, 5
+      station = create :station, description: "aa" 
+      station_id = 123 
+      order_number = "a23423"
+      station_name = "aa" 
+      label = "1231"
+      code = "sdfad"
+      commission = create :commission, price: 1
+      area = create :area,  station:station, points: points, label: label,code: code, commission: commission
+      res = json_get( areas_price_path, 
+          lantitude: test_point[:lantitude], 
+          longitude: test_point[:longitude],
+          station_id: station.id)
+
+      expect(res[:message]).to eq(I18n.t("area.commission_success"))
+      expect(res[:status]).to eq(0)
+      expect(res[:price]).to eq(1.0)
+      expect(res[:label]).to eq(label)
+      expect(res[:code]).to eq(code)
+      expect(res[:id]).to eq(area.id)
+      expect(res[:id]).not_to eq(nil)
+
+    end
+    it "address not in area" do 
+      point1 = create :point, lantitude: 40.0536, longitude: 116.297
+      point2 = create :point, lantitude: 40.0497, longitude: 116.298 
+      point3 = create :point, lantitude: 40.0464, longitude: 116.303 
+      point4 = create :point, lantitude: 40.0475, longitude: 116.311 
+      point5 = create :point, lantitude: 40.0511, longitude: 116.311 
+      point6 = create :point, lantitude: 40.0547, longitude: 116.308 
+      point7 = create :point, lantitude: 40.055, longitude: 116.307 
+
+      points = [point1,point2,point3,point4,point5,point6,point7]
+
+      test_point3 = Hash.new 
+      test_point3[:lantitude] = 40.046897
+      test_point3[:longitude] = 116.299642
+
+
+      points2 = create_list :point, 5
+      station = create :station, description: "aa" 
+      station_id = 123 
+      order_number = "a23423"
+ 
+      commission = create :commission, price: 1
+      area = create :area, station:station, points: points, label: "abc", commission: commission
+
+      res = json_get( 
+        areas_price_path, 
+        lantitude: test_point3[:lantitude], 
+        longitude: test_point3[:longitude],
+        station_id: station.id)
+ 
+      expect(res[:message]).to eq(I18n.t("area.address_not_in_station"))
+      expect(res[:status]).to eq(3)
+      expect(res[:price]).to eq(-1)
+
+
+    end
+    it "area not exist" do 
+      point1 = create :point, lantitude: 40.0536, longitude: 116.297
+      point2 = create :point, lantitude: 40.0497, longitude: 116.298 
+      point3 = create :point, lantitude: 40.0464, longitude: 116.303 
+      point4 = create :point, lantitude: 40.0475, longitude: 116.311 
+      point5 = create :point, lantitude: 40.0511, longitude: 116.311 
+      point6 = create :point, lantitude: 40.0547, longitude: 116.308 
+      point7 = create :point, lantitude: 40.055, longitude: 116.307 
+
+      points = [point1,point2,point3,point4,point5,point6,point7]
+
+      test_point3 = Hash.new 
+      test_point3[:lantitude] = 40.046897
+      test_point3[:longitude] = 116.299642
+
+
+      points2 = create_list :point, 5
+      station = create :station, description: "aa" 
+      commission = create :commission, price: 1
+      station_id = 123 
+      order_number = "a23423"
+ 
+ 
+      res = json_get( 
+        areas_price_path, 
+        lantitude: test_point3[:lantitude], 
+        longitude: test_point3[:longitude],
+        station_id: station.id)
+      expect(res[:message]).to eq(I18n.t("area.not_exist"))
+      expect(res[:status]).to eq(2)
+      expect(res[:price]).to eq(-1)
+
+    end
+
+
+    it "station not exist" do
+      test_point3 = Hash.new 
+      test_point3[:lantitude] = 40.046897
+      test_point3[:longitude] = 116.299642
+
+      points2 = create_list :point, 5
+      station = create :station, description: "aa" 
+      commission = create :commission, price: 1
+      area = create :area, station:station, label: "abc", commission: commission
+
+      station_id = 123 
+ 
+ 
+      res = json_get( 
+        areas_price_path, 
+        lantitude: test_point3[:lantitude], 
+        longitude: test_point3[:longitude],
+        station_id: station_id)
+ 
+ 
+      expect(res[:status]).to eq(1)
+      expect(res[:message]).to eq(I18n.t("area.station_not_exist"))
+      expect(res[:price]).to eq(-1)
+
+    end
+  end 
 end
