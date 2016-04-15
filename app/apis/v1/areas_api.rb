f3918367694b18a05d135aa8f9cdbe18a26349bb
Soar <85624529@qq.com>
2016-04-15 21:28:55 +0800

    fixed the hash value get error

--- app/apis/v1/areas_api.rb
+++ app/apis/v1/areas_api.rb
@@ -12,8 +12,8 @@ class V1::AreasApi < Grape::API
     def notify user, pre_content, post_content 
       unless user.nil?
         price_url = Settings.price_url
-        RestClient.post "#{price_url}/emails/area", user_id: user.id, 
-          user_name: user.name, pre_content: pre_content, post_content:post_content  
+        RestClient.post "#{price_url}/emails/area", user_id: user[:id], 
+          user_name: user[:name], pre_content: pre_content, post_content:post_content  
       end 
     end
     def log_create_info cookie_value, params 
