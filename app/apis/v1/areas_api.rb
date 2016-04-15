e605556e1766dfe9670a44c08ac40cd3be91ec59
Soar <85624529@qq.com>
2016-04-15 21:24:26 +0800

    [bug] fix var mission
    missing  url

--- app/apis/v1/areas_api.rb
+++ app/apis/v1/areas_api.rb
@@ -11,6 +11,7 @@ class V1::AreasApi < Grape::API
     end
     def notify user, pre_content, post_content 
       unless user.nil?
+        price_url = Settings.price_url
         RestClient.post "#{price_url}/emails/area", user_id: user.id, 
           user_name: user.name, pre_content: pre_content, post_content:post_content  
       end 
