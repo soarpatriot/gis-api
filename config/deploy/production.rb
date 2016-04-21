c39ea80972b73efc14d02d2644bd0823ce0846fa
Soar <85624529@qq.com>
2016-04-21 17:19:28 +0800

    [bug] change the production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,8 @@ set :server_name, "10.230.3.181"
 set :server_domain_name, "api-commission.wuliusys.com"
 set :node_count, 8
 
-set :repo_url, "https://github.com/soarpatriot/gis-api.git"
+set :repo_url, 'git@git.rfdoa.cn:java/git-api.git'
+
 set :branch, "master"
 set :deploy_to, "/data/www/gis-api"
 
