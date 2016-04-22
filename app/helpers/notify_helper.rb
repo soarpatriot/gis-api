7c397655d6aa42e69e5829c03e118395fd618cd5
Soar <85624529@qq.com>
2016-04-22 22:14:41 +0800

    [bug] fixed the email not send

--- app/helpers/notify_helper.rb
+++ app/helpers/notify_helper.rb
@@ -1,9 +1,63 @@
 module NotifyHelper
-    def notify_area user, pre_content, post_content 
+    include AccessHelper
+    def user_info cookie_value
+      unless cookie_value.nil?
+        price_url = Settings.price_url
+        user = RestClient.get "#{price_url}/users/cookie?cookie_value=#{cookie_value}"
+        user_hash = JSON.parse user, symbolize_names: true 
+        user_hash
+        
+      end
+    end
+ 
+    def notify_area user, params, area, opt_type 
       unless user.nil?
         price_url = Settings.price_url
-        RestClient.post "#{price_url}/emails/area", user_id: user[:id], user_name: user[:name], pre_content: pre_content, post_content:post_content  
+        after_price = Commission.find(params[:commission_id]).try(:price)
+        station_name = area.try(:station).try(:description)
+        begin 
+          Thread.new do
+            result = RestClient.post "#{price_url}/emails/area/note", 
+              {opt_type: opt_type,
+              user_id: user[:id],
+              user_name: user[:name], 
+              user_code: user[:code], 
+              station_name: station_name,
+              area_name_before: area.label, 
+              area_name_after: params[:label], 
+              area_code_before: area.code, 
+              area_code_after: params[:code], 
+              area_mian_before: area.mian, 
+              area_mian_after: params[:mian], 
+              area_price_before: area.try(:commission).try(:price), 
+              area_price_after: after_price,
+              update_at_before: area.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+              update_at_after: Time.now.strftime('%Y-%m-%d %H:%M:%S')}.to_json,
+              content_type: :json,
+              accept: :json
+            logger.info result.force_encoding('utf-8').encode
+          end
+        rescue  Exception => e
+          logger.info  "exception e:  #{e}"
+        end
       end 
+    end
+    def log_create_info cookie_value, params, area
+      user = user_info cookie_value
+      commission = Commission.find(params[:commission_id]) unless params[:commission_id].nil?
+      message =  "create new area by: #{user},  params: #{params[:commission_id]}, price: #{commission.try(:to_json)}"
+      logger.info message
+      notify_area user, params, area, 1 
+    end
+
+    def log_change_area cookie_value, params, area 
+      user = user_info cookie_value
+      commission = Commission.find(params[:commission_id]) unless params[:commission_id].nil?
+      pre_content =  "update area by: #{user}, old value: area_id: #{area.to_json}, price: #{area.commission.try(:to_json)}, " 
+      post_content = "  params: #{params}, price: #{commission.try(:to_json)}  "
+
+      logger.info pre_content + post_content
+      notify_area user, params, area, 2
     end
  
 end
--- spec/helpers/notify_helper_spec.rb
+++ spec/helpers/notify_helper_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 include NotifyHelper
 describe NotifyHelper, type: :helper do 
-  context "auth" do 
-    it "sort_params test" do 
+ context "sending email" do 
+    it "send" do 
       user = Hash.new
+      params = Hash.new
       user[:id] = 1212
       user[:name] = "sdfas"
       user[:code] = "sdfasdf"
-      notify_area user, "ss", "sss" 
-       
-      
+      commission = create :commission, price: 2.1
+      params[:commission_id] = commission.id
+      station = create :station, description:"aa"
+      area = create :area, station: station, commission: commission       
+
+      notify_area user,  params,area, 1
+ 
     end
   end
 end
